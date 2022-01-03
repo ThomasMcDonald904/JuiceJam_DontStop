3f25e93e2cb8a7f47d76bfbd7dcd11d51f1c4137
Thomas McDonald <naynaytom3000@gmail.com>
2022-01-03 15:44:34 -0500

    Enemy goes to desired wait position

--- Enemy.gd
+++ Enemy.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var gravity = 2000
 
 func _physics_process(delta):
-	velocity += Vector2(-2, 0)
-	velocity.y += gravity * delta
-	velocity.y = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP, false).y
+	if position.x >= 1301:
+		velocity += Vector2(-2, 0)
+		velocity = move_and_slide_with_snap(velocity.rotated(rotation), Vector2.ZERO, -transform.y, true, 4, deg2rad(50))
+
