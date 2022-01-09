b27993dfc95e43c140d9508aa9a9bff0aa987b4f
Thomas McDonald <naynaytom3000@gmail.com>
2022-01-09 03:08:46 -0500

    Made Motorcycle Soldier
    Added 2 to level 3

--- Scripts/MachineGunBullet.gd
+++ Scripts/MachineGunBullet.gd
@@ -4,9 +4,19 @@ var maxDistance = 10
 var distance = 0
 
 func _ready():
-	apply_impulse(Vector2(), Vector2(1000, 0).rotated(rotation))
+	apply_impulse(Vector2(), Vector2(1500, 0).rotated(rotation))
 
 func _process(delta):
 	distance += 10 * delta
 	if distance >= maxDistance:
+		queue_free()
+
+
+func _on_MachineGunBullet_area_entered(area):
+	if area.get_owner().name != "Player":
+		queue_free()
+
+
+func _on_MachineGunBullet_body_entered(body):
+	if body.name != "Player":
 		queue_free()
