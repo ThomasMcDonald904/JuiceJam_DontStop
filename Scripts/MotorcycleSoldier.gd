7b92572bd0d93ca4437f08b04d228eae4e6a7237
Benjamin McDonald <streetsamurai2001@gmail.com>
2022-01-11 10:02:42 -0500

    Added fix for motorcycles

--- Scripts/MotorcycleSoldier.gd
+++ Scripts/MotorcycleSoldier.gd
@@ -5,12 +5,15 @@ var done = false
 var rng = RandomNumberGenerator.new()
 var bullet: PackedScene = preload("res://Props/MotorcycleSoldierBullet.tscn")
 signal died(id)
+var inBlastWave = false
+var inShrapnel = false
+export var life_points = 5
 
 func _ready():
 	connect("died", $"..", "_on_Enemy_died")
 	rng.randomize()
 	arrivalSpeed -= rng.randi_range(0, 20)
-	$Control/CenterContainer/LifePoints.text = str(5)
+	$Control/CenterContainer/LifePoints.text = str(life_points)
 
 
 
@@ -23,9 +26,6 @@ func _physics_process(delta):
 		$Timer.wait_time = rng.randf_range(0.1, 0.5)
 		$Timer.start()
 		done = true
-	if int($Control/CenterContainer/LifePoints.text) <= 0:
-		emit_signal("died", get_instance_id())
-		queue_free()
 
 
 
@@ -41,4 +41,37 @@ func _on_Timer_timeout():
 
 func _on_MotorcycleSoldierArea_area_entered(area):
 	if area.name == "MachineGunBullet":
-		$Control/CenterContainer/LifePoints.text = str(int($Control/CenterContainer/LifePoints.text) - 1)
+		bullet_damage()
+	if area.name == "BlastWave":
+		var shell:Node = area.get_parent().get_parent()
+		shell.connect("explode", self, "blast_wave_damage")
+		inBlastWave = true
+	if area.name == "Shrapnel":
+		var shell:Node = area.get_parent().get_parent()
+		shell.connect("explode", self, "shrapnel_damage")
+		inShrapnel = true
+
+func blast_wave_damage():
+	if inBlastWave == true:
+		life_points -= 9
+		$Control/CenterContainer/LifePoints.text = str(life_points)
+		inBlastWave = false
+		if life_points <= 0:
+			emit_signal("died", get_instance_id())
+			queue_free()
+
+func bullet_damage():
+	life_points -= 1
+	$Control/CenterContainer/LifePoints.text = str(life_points)
+	if life_points <= 0:
+			emit_signal("died", get_instance_id())
+			queue_free()
+
+
+func shrapnel_damage():
+	if inShrapnel == true:
+		life_points -= 6
+		$Control/CenterContainer/LifePoints.text = str(life_points)
+		if life_points <= 0:
+			emit_signal("died", get_instance_id())
+			queue_free()
